0eab32890b6c642dd780eae7561b10b8f7a81c78
bavanDA <56412698+bavanDA@users.noreply.github.com>
2023-07-13 05:32:43 +0330

    add traindata

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 GDRIVE_PATH = "gdrive/MyDrive/NLP/github"
 BASE_PATH = "data/"
 SENTENCE_BROKEN_PATH = "data/sentencebroken"
-CATEGORIES = ['action', 'adventure', 'rpg',
-              'strategy', 'simulation', 'sports_and_racing']
+CATEGORIES = ['action', 'adventure', 'rpg','strategy', 'simulation', 'sports_and_racing','all']
+
 WORD2VEC_ITER = 10000
